48f65e93b59680c2afe80b6347cf1e05b8b2b213
michelchouinard <michel.chouinard@gmail.com>
2014-12-17 15:20:51 -0500

    Menage

--- dionchou_kub/wp-content/themes/briefcase/includes/extensions/like/like_ajax.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include("includes/extensions/like/like.class.php");
-
-if (isset($_POST['vote']) && isset($_POST['post']) && $_POST['post'] != "" && $_POST['vote'] != "") {
-    $like = new Like();
-    $like->addToFile($_POST['post'], $_POST['vote']);
-    exit();
-}
