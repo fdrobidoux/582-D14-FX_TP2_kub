aedca8c199e3970323a411a2cd8e9596b9cb66e0
diglididudeng <diglididude.ng@gmail.com>
2014-12-20 07:57:11 -0500

    Fixé containers et rows dans pages génériques

--- dionchou_kub/wp-content/themes/kartoffel-und-bier/header.php
+++ dionchou_kub/wp-content/themes/kartoffel-und-bier/header.php
@@ -17,21 +17,23 @@
 	<header>
 		<nav class="navbar navbar-default navbar-inverse navbar-fixed-top navbar-lg" role="navigation">
 			<div class="container">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
-				</div>
+				<div class="row">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
+					</div>
 
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse navbar-ex1-collapse">
-					<?php wp_bootstrap_main_nav(); ?>
-				</div><!-- /.navbar-collapse -->
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse navbar-ex1-collapse">
+						<?php wp_bootstrap_main_nav(); ?>
+					</div><!-- /.navbar-collapse -->
+				</div>
 			</div>
 		</nav>
 	</header>
