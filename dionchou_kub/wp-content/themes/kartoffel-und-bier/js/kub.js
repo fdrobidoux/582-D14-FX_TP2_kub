abec3334a3a824cf2aa5e0e1c7ea7f4a4e761498
diglididudeng <diglididude.ng@gmail.com>
2014-12-21 19:53:26 -0500

    Spinner pour les qte de produits

--- dionchou_kub/wp-content/themes/kartoffel-und-bier/js/kub.js
+++ dionchou_kub/wp-content/themes/kartoffel-und-bier/js/kub.js
@@ -29,4 +29,26 @@ $(function(){
         }
     );
 
+    $.widget('ui.customspinner', $.ui.spinner, {
+        widgetEventPrefix: $.ui.spinner.prototype.widgetEventPrefix,
+        _buttonHtml: function () { // Remove arrows on the buttons
+            return '' +
+                '<a class="ui-spinner-button ui-spinner-up ui-corner-tr">' +
+                '<span class="ui-icon ' + this.options.icons.up + '"></span>' +
+                '</a>' +
+                '<a class="ui-spinner-button ui-spinner-down ui-corner-br">' +
+                '<span class="ui-icon ' + this.options.icons.down + '"></span>' +
+                '</a>';
+        }
+    });
+
+    $('.spinner').customspinner({
+        min: -99,
+        max: 99
+    }).on('focus', function () {
+        $(this).closest('.ui-spinner').addClass('focus');
+    }).on('blur', function () {
+        $(this).closest('.ui-spinner').removeClass('focus');
+    });
+
 });
